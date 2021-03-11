048893b8b65b8210cc3d147ea42b73a88e4927d9
Hector Ruiz <hruiz.com@gmail.com>
2021-03-11 18:46:03 -0500

    conectando sockets con front

--- servidor.js
+++ servidor.js
@@ -34,7 +34,7 @@ io.on('connection', socket => {
         console.log("Cantidad de salas: " + salas.length)
         if (salas.length === 0) {
             console.log("Crea la primera sala")
-            salas.push({ id: sala, playerA: nick })
+            salas.push({ id: sala, playerA: nick, dos: false, turno: true })
         } else {
             let encuentra = false
             salas.forEach((salon, index) => {
@@ -46,26 +46,34 @@ io.on('connection', socket => {
             })
             if (!encuentra) {
                 console.log("Crea una sala nueva B")
-                salas.push({ id: sala, playerA: nick })
+                salas.push({ id: sala, playerA: nick, dos: false, turno: true })
                 SALA = salas.length - 1
             } else {
                 if (salas[SALA].playerB) {
                     console.log("Solo pueden haber 2 jugadores.")
                 } else {
                     salas[SALA].playerB = nick
+                    salas[SALA].dos = true
                 }
             }
 
         }
-        console.log("SALAS: ", salas)
-        console.log(salas[SALA])
+        //console.log("SALAS: ", salas)
+        //console.log(salas[SALA])
         //console.log("Usuario " + nick + " conectado a la sala: " + sala)
         socket.join(sala)
+
+        socket.to(sala).emit("sala", {
+            dos: salas[SALA].dos,
+            turno: true
+        })
+
+
 
     })
 
     socket.on("sala", (datos) => {
-        const { sala, fire, listo, destruido, afecta } = datos;
+        const { sala, fire, listo, destruido, afecta, fin } = datos;
         //console.log("Llego a " + sala)
         //console.log("LOS DATOS SON: ", datos)
         let SALA = 0;
@@ -78,32 +86,53 @@ io.on('connection', socket => {
             }
         })
 
+
+        salas[SALA].turno = !salas[SALA].turno
+
+        console.log("Turno: ", salas[SALA].turno)
+
+
         if (listo) {
             console.log("listo")
+            salas[SALA].turno = true
             socket.to(sala).emit("sala", {
                 listo: true,
-                turno
+                turno: salas[SALA].turno
             })
         } else if (destruido) {
             console.log("destruido")
             socket.to(sala).emit("sala", {
                 destruido,
-                turno
+                turno: salas[SALA].turno
             })
         } else if (fire) {
-            console.log("fire")
+            console.log("fire", fire)
             socket.to(sala).emit("sala", {
                 fire,
-                turno
+                turno: salas[SALA].turno
             })
         } else if (afecta) {
+
             console.log("afecta")
             socket.to(sala).emit("sala", {
                 afecta,
-                turno
+                turno: salas[SALA].turno
+            })
+        } else if (fin) {
+            console.log("fin")
+            socket.to(sala).emit("sala", {
+                fin,
+                turno: salas[SALA].turno
             })
         }
+
+
+
     })
+
+
+
+
 });
 
 servidor.listen(3001, () => {
